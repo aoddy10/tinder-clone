d9fbf0ad4715fc34dcaa11756e74d731a7f49be1
aoddy10 <vobi69p@gmail.com>
2024-09-19 09:10:10 +1000

    Setup app project with tailwind

---
+++ app/App.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+    return (
+        <View className="flex h-full justify-center items-center bg-gray-200">
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
